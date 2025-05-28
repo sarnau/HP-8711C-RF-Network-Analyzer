9aad22b35fc7ca8944d1508d41c8b08c50071322
Markus Fritze <github@sarnau.com>
2025-05-28 16:25:13 +0200

    More work

--- Firmware Update C.04.52/c-04-52/unzip.py
+++ Firmware Update C.04.52/c-04-52/unzip.py
@@ -1,9 +1,15 @@
 import gzip
+import struct
 
 data = bytearray()
 for idx in range(7):
 	with gzip.open('C871XC_%d' % idx, 'rb') as f:
-		data += bytearray(f.read())[0x40:]
+		block = bytearray(f.read())
+		versionA,headerVersion = struct.unpack('>HH', block[:4])
+		headerSize = 0x40
+		if headerVersion == 0x010B or headerVersion == 0x10D or headerVersion == 0x010E:
+			headerSize = 0x1000 # This seems only to be used during development for RAM based firmware
+		data += block
 
 with open('C871XC_0x40000000.bin', 'wb') as output_file:
 	output_file.write(data)
